7b489a27bf4b2149efb94430917ebc196cc93882
fai_14 <nariratfai46@gmail.com>
2025-08-13 17:14:11 +0700

    update table

--- src/components/loginpage.jsx
+++ src/components/loginpage.jsx
@@ -21,7 +21,7 @@ function LoginPage() {
         const password = e.target.password.value;
 
         try {
-            const response = await fetch("http://localhost:3001/login", {
+            const response = await fetch("http://localhost:3001/users", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password })
